abaf10a5d28e12f02c8b049a8435b8af52e8aa23
orichey <owenqrichey@gmail.com>
2025-07-23 16:38:07 -0500

    no clouds, magmx title is working

--- src/graphics.c
+++ src/graphics.c
@@ -2068,7 +2068,7 @@ const u16 gMonIconPalettes[][16] =
 };
 
 const u16 gTitleScreenBgPalettes[]         = INCBIN_U16("graphics/title_screen/pokemon_logo.gbapal",
-                                                        "graphics/title_screen/wintertilemappalette.gbapal"); //TITLE SCREEN PALETTE UPDATE HERE NOW
+                                                        "graphics/title_screen/magmxpalette.gbapal"); //TITLE SCREEN PALETTE UPDATE HERE NOW
 const u16 gTitleScreenEmeraldVersionPal[]  = INCBIN_U16("graphics/title_screen/emerald_version.gbapal");
 const u32 gTitleScreenCloudsTilemap[]      = INCBIN_U32("graphics/title_screen/clouds.bin.lz");
 const u32 gTitleScreenPokemonLogoGfx[]     = INCBIN_U32("graphics/title_screen/pokemon_logo.8bpp.lz");
